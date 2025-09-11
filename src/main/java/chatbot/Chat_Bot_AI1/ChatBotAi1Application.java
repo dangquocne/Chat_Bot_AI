3c54a6b1af8622c15a6ee4db48ce52a08beef2b8
dangquocne <huynhdangquoctuan@gmail.com>
2025-09-11 23:33:33 +0700

    init commit

---
+++ src/main/java/chatbot/Chat_Bot_AI1/ChatBotAi1Application.java
@@ -0,0 +1,13 @@
+package chatbot.Chat_Bot_AI1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotAi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/chatbot/Chat_Bot_AI1/ChatBotAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package chatbot.Chat_Bot_AI1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
